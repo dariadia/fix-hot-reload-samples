bf16a54280dff6a787a7edd003c7bec2e5bdbb27
Daria Diachkova <daria.diachkova@mail.ru>
2022-05-12 21:39:25 +0300

    feat(app): add sample project

---
+++ js/box-creator.js
@@ -0,0 +1,10 @@
+const blueNode = document.createElement('div')
+blueNode.setAttribute('style', 'background-color: blue; padding: 20px; margin: 10px;')
+blueNode.textContent = 'This is a blue div'
+document.body.appendChild(blueNode)
+
+if (module.hot) {
+  module.hot.dispose(function() {
+    blueNode.parentNode.removeChild(blueNode)
+  })
+}
---
+++ js/index.js
@@ -0,0 +1,7 @@
+require('./box-creator')
+
+console.log('I just reloaded at', new Date())
+
+if (module.hot) {
+  module.hot.accept()
+}
