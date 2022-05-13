f8c7829f87c9541cc09097546e80249476034134
Daria Diachkova <daria.diachkova@mail.ru>
2022-05-13 08:54:28 +0300

    feat(assets): update

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ### Samples
 
-todo description
+TODO some description
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,19 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path') 
+const webpack = require('webpack') 
 
-var config = {
+module.exports = {
   context: path.join(__dirname, 'js'),
   entry: [
     'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000',
     './index.js',
   ],
   output: {
-    path: path.join(__dirname, 'www'),
+    path: path.join(__dirname, 'src'),
     filename: 'bundle.js',
     publicPath: '/assets/',
   },
   plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ]
-};
-module.exports = config;
+    new webpack.NoEmitOnErrorsPlugin(),
+  ],
+} 
